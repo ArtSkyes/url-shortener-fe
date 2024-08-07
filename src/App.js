1f73d52b0d4aa2b99d7783592ce9cd4ba38849bc
Art <honeydewart@gmail.com>
2024-08-07 09:03:22 +0800

    Update routing for Home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import ShortUrl from './pages/ShortUrl';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:shortCode" element={<ShortUrl />} />
-      </Routes>
+      <div className="min-h-screen bg-gradient-to-br from-sand to-sky">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
